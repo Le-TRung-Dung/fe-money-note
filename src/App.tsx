d84d15a6ae1ca422fe9f4b3eef4a660ec2039f40
Le Trung Dung <dung.letrung@mobifone.vn>
2026-06-04 16:19:47 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import About from "./pages/RegisterScreen";
 import LandingPage from "./pages/LandingScreen";
@@ -12,6 +12,7 @@ import SavingCreateScreen from "./pages/SavingCreateScreen";
 import ForgotPasswordScreen from "./pages/ForgotPasswordScreen";
 import AccountScreen from "./pages/AccountScreen";
 import AppShell from "./components/AppShell";
+import PublicShell from "./components/PublicShell";
 import SplashScreen from "./pages/SplashScreen";
 import ScrollToTop from "./components/ScrollToTop";
 import TransactionSearchScreen from "./pages/TransactionSearchScreen";
@@ -19,21 +20,32 @@ import TransactionListScreen from "./pages/TransactionListScreen";
 import SavingTransactionListScreen from "./pages/SavingTransactionListScreen";
 import SavingTransactionSearchScreen from "./pages/SavingTransactionSearchScreen";
 import NotificationScreen from "./pages/NotificationScreen";
-import PublicShell from "./components/PublicShell";
 
 const App: React.FC = () => {
   return (
     <>
       <ScrollToTop />
+
       <Routes>
+        {/* Màn public: không có BottomNav */}
         <Route element={<PublicShell />}>
           <Route path="/" element={<SplashScreen />} />
-
           <Route path="/about" element={<About />} />
           <Route path="/landing" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
+        </Route>
+
+        {/* Màn chính: có BottomNav + Onboarding */}
+        <Route element={<AppShell />}>
+          <Route path="/dashboard" element={<DashboardScreen />} />
+
+          <Route path="/transactions" element={<TransactionListScreen />} />
+          <Route
+            path="/transactions/search"
+            element={<TransactionSearchScreen />}
+          />
           <Route
             path="/transactions/create"
             element={<TransactionCreateScreen />}
@@ -42,34 +54,24 @@ const App: React.FC = () => {
             path="/transactions/:id/edit"
             element={<TransactionCreateScreen />}
           />
+
+          <Route path="/savings" element={<SavingScreen />} />
           <Route
-            path="/transactions/search"
-            element={<TransactionSearchScreen />}
+            path="/savings/transactions"
+            element={<SavingTransactionListScreen />}
           />
           <Route
             path="/savings/transactions/search"
             element={<SavingTransactionSearchScreen />}
           />
-
-          <Route path="/notifications" element={<NotificationScreen />} />
-        </Route>
-
-        <Route element={<AppShell />}>
-          <Route path="/dashboard" element={<DashboardScreen />} />
-          <Route path="/transactions" element={<TransactionListScreen />} />
-          <Route
-            path="/savings/transactions"
-            element={<SavingTransactionListScreen />}
-          />
-
-          <Route path="/savings" element={<SavingScreen />} />
           <Route path="/savings/create" element={<SavingCreateScreen />} />
           <Route path="/savings/:id/edit" element={<SavingCreateScreen />} />
 
+          <Route path="/notifications" element={<NotificationScreen />} />
           <Route path="/account" element={<AccountScreen />} />
         </Route>
 
-        <Route path="*" element={<h2>404 - Không tìm thấy trang</h2>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
